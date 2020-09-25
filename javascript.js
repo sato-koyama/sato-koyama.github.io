47c354f2e8eeb7778ce0be051745bcdb1e16e76e
sato_uwu <35643696+sato-koyama@users.noreply.github.com>
2020-09-26 00:10:00 +1200

    REWORK

--- javascript.js
+++ javascript.js
@@ -1,24 +1,10 @@
-// auto dark-mode
-console.log("checking mode.");
-var date = new Date();
-var time = date.getHours();
-console.log("datetime: " + date);
-console.log("\n");
 
-// text color convert
-if (time>=18) {
-	console.log("initiating dark mode.");
-	document.getElementById("style").href = "dark_master_css.css";
-	console.log("dark mode activated.");
-} else {
-	console.log("daytime.");
-}
-// title
+
 var data = [
-"Free Office Subscription", "Power Play: car wash", "Noodelay", 
-"A large suc", "Forever ClawBot!", "Machine Learning", 
-"No Don't", "yesn't", "I hope so", "Such tasty cok", "Zacian",
-"It works!", "Where the wind blows"
+"Free Office Subscription", "Power Play: car wash", "Noodelay",
+"Forever ClawBot!", "Machine Learning", 
+"No Don't", "I hope so", "peko!",
+"It works!", "Red kinda sus", "yubi yubi!"
 ];
-var one = data[Math.floor(Math.random() * data.length)];
-document.getElementById("title").innerHTML = one;
+var title = data[Math.floor(Math.random() * data.length)];
+document.getElementById("title").innerHTML = title;
