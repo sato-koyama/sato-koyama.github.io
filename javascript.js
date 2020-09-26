624b2f1f0c70b47d5d904de17def77b0ae508a80
sato_uwu <35643696+sato-koyama@users.noreply.github.com>
2020-09-26 14:49:38 +1200

    yyay

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
+// Top to Bot Function
+function topFunc() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+function botFunc() {
+	window.scrollTo(0, document.body.scrollHeight);
+}
+
+// Title
+
 var data = [
 "Free Office Subscription", "Power Play: car wash", "Noodelay",
 "Forever ClawBot!", "Machine Learning", 
@@ -7,3 +19,4 @@ var data = [
 var title = data[Math.floor(Math.random() * data.length)];
 document.getElementById("title").innerHTML = "<i>" + title + "</i>";
 document.getElementById("web_title").innerHTML = title;
+
